1ba6c08b64aa228f619fab4d585f2b12c9b0e7d5
HackerU <HackerU@hackerus-mac-2.local>
2017-02-14 15:34:11 +0200

    Initial Commit

---
+++ LastLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LastLocation
+//
+//  Created by HackerU on 14/02/2017.
+//  Copyright © 2017 HackerU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
